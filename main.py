51e633c21714fd5801f2bfaf1abebbc76743aaf9
cdub706 <williams.chauncey@gmail.com>
2025-11-19 22:27:23 -0500

    feat: introduce `CircleShape` class and add game clock with delta time calculation.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -16,9 +18,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        time_elapsed = clock.tick(60)
+        dt = time_elapsed / 1000
 
         screen.fill("black")
-        pygame.display.flip
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
